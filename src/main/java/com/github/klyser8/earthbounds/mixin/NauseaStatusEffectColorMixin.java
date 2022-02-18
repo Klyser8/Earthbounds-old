fdb260adf0d0b628f8920e89ea2cd09459ed1b92
Klyser8 <tomrom99@gmail.com>
2022-02-18 21:04:03 +0000

    1.0.5 preparation

---
+++ src/main/java/com/github/klyser8/earthbounds/mixin/NauseaStatusEffectColorMixin.java
@@ -0,0 +1,20 @@
+package com.github.klyser8.earthbounds.mixin;
+
+import net.minecraft.entity.effect.StatusEffects;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Slice;
+
+@Mixin(StatusEffects.class)
+public abstract class NauseaStatusEffectColorMixin {
+
+    @ModifyArg(method = "<clinit>", slice = @Slice(from = @At(value = "CONSTANT", args = "stringValue=nausea")),
+            at = @At(value = "INVOKE",
+                    target = "Lnet/minecraft/entity/effect/StatusEffect;<init>(Lnet/minecraft/entity/effect/StatusEffectCategory;I)V",
+                    ordinal = 0))
+    private static int changeColor(int color) {
+        return 13825638;
+    }
+
+}
